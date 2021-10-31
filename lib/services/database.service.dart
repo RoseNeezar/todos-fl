4124351fb633a9e13e55eb9a67f68a77581dcc92
Neezar <rforlyffe@gmaicl.com>
2021-10-31 17:06:10 +0800

    add db structure class

---
+++ lib/services/database.service.dart
@@ -0,0 +1,41 @@
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+class DatabaseService {
+  static const DatabaseService instance = DatabaseService._();
+
+  const DatabaseService._();
+
+  static const String _todoTable = 'todos_table';
+  static const String _colId = 'id';
+  static const String _colName = 'name';
+  static const String _colDate = 'date';
+  static const String _colPriorityLevel = 'priority_level';
+  static const String _colCompleted = 'completed';
+
+  static Database? _db;
+
+  Future<Database> get db async {
+    _db ??= await _openDb();
+    return _db!;
+  }
+
+  Future<Database> _openDb() async {
+    final dir = await getApplicationSupportDirectory();
+    final path = dir.path + '/todo_list.db';
+    final todoListDB = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute(''' 
+        CREATE TABLE $_todoTable (
+          $_colId INTEGER PRIMARY KEY AUTOINCREMENT,
+          $_colName TEXT,
+          $_colDate TEXT,
+          $_colPriorityLevel TEXT,
+          $_colCompleted INTEGER
+        ) 
+        ''');
+    });
+    return todoListDB;
+  }
+}
